4e5010d12c153f4c355b48a91f1451aaac864e84
Alejandro Jimenez <ejimenez164@unab.edu.co>
2026-08-20 11:46:01 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class );
 
-Route::get('/product', [ProductController::class , 'index']);
+Route::prefix('product')->controller(ProductController::class)->group(function () {
+    
 
-
-Route::get('/product/create', [ProductController::class , 'create']);
-
-
-
-Route::get('/product/{idProduct}', [ProductController::class , 'show']);
+Route::get('/product',  'index');
+Route::get('/product/create',  'create');
+Route::get('/product/{idProduct}',  'show');
+});
